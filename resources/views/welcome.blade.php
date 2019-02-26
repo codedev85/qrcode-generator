98f2d070150f5c4fca7a6ef512c5b5e17101ddd5
techHQ <30844218+techHQ@users.noreply.github.com>
2019-02-26 22:19:26 +0000

    changing the fro t page tetx

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ORCode GENERATOR
                 </div>
 
                 <div class="links">
