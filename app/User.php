12a1319ebf37f292d7016e41a9dcad09bf36654e
techHQ <30844218+techHQ@users.noreply.github.com>
2019-03-04 15:03:53 +0000

    adding changes made to the api

--- app/User.php
+++ app/User.php
@@ -3,12 +3,11 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable ,HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
